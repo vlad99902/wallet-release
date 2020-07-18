a23148ca58ed1a3fb1c48cbcc3f3a3e983bb8b49
bacebu4 <vasua14735@icloud.com>
2020-07-18 11:31:14 +0300

    better cards and expense containers

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,14 +7,35 @@
       <div class="card-1">
         <img src="../../statics/card1.png" alt=""> 
         <div class="card-content">
+
           <span>
-            Avaliable <br>
+            Avaliable   <br>
           </span>
+
           <span class="avaliable-money">
             $564.4
           </span>
+
+          <!-- progress bar -->
+          <span class="bar">
+            <q-linear-progress
+              size="7px"
+              class="bar"
+              color="secondary"
+              :value="progress" />
+          </span>
+
+          <!-- budget analitycs -->
+          <div class="spent-limit row">
+            Spent: $12.43
+            <q-space></q-space>
+            Daily limit: $12.43
+          </div>
+
         </div>
       </div>
+
+      <!-- second card -->
       <div class="card-2">
         <img src="../../statics/card2.png" alt="">
         <div class="card-content">
@@ -39,7 +60,7 @@
 
     <div class="date-container">
       <div class="month">
-      SEPTEMBER, 29
+      SEPT, 29
       </div>
       <div class="total">
         TOTAL: $12.43
@@ -78,7 +99,7 @@
         </div>
 
         <div class="purchase-name">
-          <span>Buy milk when you drink a lot of beer</span>
+          <span>Buy milk when you drink a lot of beer and check the awesome work of flexboxes</span>
         </div>
 
         <div class = "purchase-sum"> 
@@ -96,38 +117,41 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+     return {
+        progress: 0.4 //progress of progress bar
+     }
+  }
 }
 </script>
 
 <style lang="scss">
 
+//main heading of the page
 .heading {
   font-size: 3.375rem;
   font-weight: 700;
   padding-left: 2rem;
 }
 
+//toda's date
 .date {
   font-size: 1.4rem;
   padding-left: 2rem;
   font-weight: 300;
 }
 
+//card's container
 .cards {
   overflow-x: scroll;
   display: flex;
-  height: 16.5rem;
   padding-top: 1.4rem;
 }
 
+//disable display of scroll for cards
 .cards::-webkit-scrollbar {
   display: none;
-}
-
-.card-1 img {
-  width: 20rem;
-  height: 15rem;
 }
 
 .card-1 {
@@ -139,33 +163,86 @@ export default {
   margin-left: 1rem;
 }
 
+.card-1 img {
+  width: 80vw;
+  max-width: 300px;
+
+}
+
 .card-2 img {
-  width: 20rem;
-  height: 15rem;
+  width: 80vw;
+  max-width: 300px;
   margin-right: 2rem;
 }
 
+//container inside of cards
 .card-content {
-  line-height: 2.3rem;
+  line-height: 10vw;
   color: $for-white;
-  font-size: 2rem;
+  font-size: 9vw;
   font-weight: 700;
-  padding-left: 2rem;
+  padding-left: 8vw;
   position: absolute;
   top: 0%;
-  padding-top: 1.8rem;
-  margin-right: 2rem;
+  padding-top: 7vw;
+  margin-right: 55px;
 }
 
 .avaliable-money {
   font-weight: 300;
 }
 
+//styling progress bar
+.q-linear-progress {
+  border-radius: 9px;
+}
+
+.q-linear-progress__model--determinate {
+  border-radius: 18px;
+}
+
+.bar {
+  margin-top: 3vw;
+  width: 63vw;
+  max-width: 233px;
+}
+
+.spent-limit {
+  font-weight: 700;
+  font-size: 3.2vw;
+}
+
 .week-analitycs {
   font-weight: 700;
-  font-size: .875rem;
-  line-height: 1.4rem;
-  padding-top: 1rem;
+  font-size: 3.2vw;
+  line-height: 4vw;
+  margin-top: 2vw;
+}
+
+//fixing how cards look for displays more than 372px width
+@media screen and (min-width: 372px) {
+  .card-content {
+    font-size: 33px;
+    line-height: 37px;
+    padding-top: 26px;
+    padding-left: 30px;
+    margin-right: 60px;
+  }
+
+  .week-analitycs {
+    font-size: 12px;
+    line-height: 15px;
+    margin-top: 9px;
+  }
+
+  .spent-limit {
+    font-size: 12px;
+  }
+
+  .bar {
+  margin-top: 15px;
+}
+
 }
 
 .recent {
@@ -177,18 +254,18 @@ export default {
 
 .month {
   font-weight: 700;
-  padding-left: 1rem;
   color: $secondary;
-  font-size: 0.875rem;
+  font-size: 0.75rem;
 }
 
 .date-container {
   display: flex;
-  padding-left: 1rem;
+  padding-left: 2rem;
   padding-right: 1rem;
   padding-top: 0.4rem;
   align-items: flex-end;
   justify-content: space-between;
+  max-width: 600px;
 }
 
 .date-2-month {
@@ -204,6 +281,11 @@ export default {
 
 //vlad
 //one of element in recent 
+
+.purchase-container {
+  max-width: 600px;
+}
+
 .purchase {
   display: grid;
   grid-template-columns: 10% 70% 20%;
@@ -212,59 +294,66 @@ export default {
   grid-gap: .1rem;
 
   margin: 0.6rem 2rem;
-  padding: 0.65rem;
+  //padding: 0.65rem;
+  padding: 1rem;
   border-radius: 0.7rem;
   background-color: $for-white;
   box-shadow: 0 0.2rem 1.2rem -0.2rem rgba(0,0,0,.1);
 }
 
 //tag in recent purchase
-.purchase-tag{
+.purchase-tag {
   justify-self: left;
   align-self: start;
 
   grid-row: 1/3;
   grid-column: 1/2;
+
+  padding-top: .15rem;
+}
+
+//purcahse tag recent only img
+.purchase-tag img {
+  width: 1.2rem;
+  height: 1.2rem;
 }
 
 //
-.purchase-name{
+.purchase-name {
   justify-self: left;
   align-self: start;
 
   grid-row: 1/3;
   grid-column: 2/3;
+  margin-right: 2rem;
 
-  font-weight: 600;
+  font-weight: 400;
 }
 
 //
-.purchase-sum{
+.purchase-sum {
   grid-row: 1/2;
   grid-column: 3/4;
 
-  justify-self: left;
+  justify-self: right;
   align-self: start;
+  padding-right: 1rem;
 
   font-weight: 300;
 }
 
 //
-.purchase-more{
+.purchase-more {
   grid-row: 2/3;
   grid-column: 3/4;
 
-  justify-self: left;
-  align-self: start;
+  justify-self: right;
+  align-self: end;
+  padding-right: 1rem;
 
   color: $secondary;
   font-size: 0.6rem;
-  font-weight: 600;
+  font-weight: 700;
 }
 
-//purcahse tag recent only img
-.purchase-tag img{
-  width: 1.2rem;
-  height: 1.2rem;
-}
 </style>
