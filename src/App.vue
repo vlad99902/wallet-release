173dedad15b5edb2a91ab080a9ba9b44a4937a1f
bacebu4 <vasua14735@icloud.com>
2020-07-19 22:06:51 +0300

    started dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  <div id="q-app">
+  <div id="q-app" class="">
     <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+     return {
+        showNightMode: true
+     }
+  }
 }
 </script>
+
+<style lang="scss">
+
+  .night  {
+  padding: 0;
+  padding-top: 2rem;
+  box-sizing: border-box-dark;
+  margin: 0;
+  height: 100vh;
+  width: 100vw;
+
+  font-family: 'Lato';
+  font-weight: 300;
+  font-size: 16px;
+
+  color: $primary-dark;
+  background: $for-background-dark;
+}
+
+</style>
