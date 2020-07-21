acf394ea340c42aeaeadb0499882eb66bc7322b8
vlad99902 <legkovv@mail.ru>
2020-07-21 03:13:01 +0300

    design add, adding algorytm first

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,66 @@
 <template>
-<!-- applying dark mode here -->
-  <div id="q-app" class="main-app" :class="settings.showDarkMode ? 'dark' : ''"
-  >
-    <router-view :class="settings.showAddExpense ? 'blur-overlay' : ''"  />
+  <!-- applying dark mode here -->
+  <div id="q-app" class="main-app" :class="settings.showDarkMode ? 'dark' : ''">
+    <router-view :class="settings.showAddExpense ? 'blur-overlay' : ''" />
 
     <q-dialog v-model="settings.showAddExpense">
-      
       <q-card>
-        <q-card-section>
+        <form @submit.prevent="submitForm">
+          <q-card-section>
             <div class="header">
-          <div class="header-element">Add</div>
-          <div class="header-element">Split</div>
+              <div class="header-element">Add</div>
+              <div class="header-element">Split</div>
             </div>
-          <!-- <q-btn icon="close" flat round dense @click="setShowAddExpense()" /> -->
-        </q-card-section>
 
-        <q-card-section>
-          <div class="input-field">
-            <div class="input-field-text">Name</div>
-          </div>
+            <div class="input-fiels-container">
+              <input
+                v-model="expanseToSubmit.purchases.name"
+                :rules="[val => !!val || 'Field is required']"
+                ref="name"
+                autofocus
+                type="text"
+                placeholder="Name"
+                class="input-field"
+              />
 
-          <div class="input-field">
-            <div class="input-field-text">Date</div>
-          </div>
+              <input
+                
+                type="text"
+                placeholder="Date"
+                
+                class="input-field" />
 
-          <div class="input-field">
-            <div class="input-field-text">Start writing or choose tag</div>
-            <hr> 
-            <div class="input-field-text">Tags pictures here</div>
-          </div>
+              <input
+                v-model="expanseToSubmit.purchases.category"
+                type="text"
+                placeholder="Start writing tag"
+                class="input-field" />
 
-          <div class="input-field">
-            <div class="input-field-text">$$$</div>
-          </div>
+              <input
+                v-model="expanseToSubmit.purchases.cost"
+                type="text"
+                placeholder="$$$"
+                class="input-field" />
 
-          <div class="input-field">
-            <div class="input-field-text">Description</div>
-          </div>
+              <input
+                v-model="expanseToSubmit.purchases.description"
+                type="text"
+                placeholder="Description"
+                class="input-field" />
+            </div>
 
-          <div class="button-add">
-            <div>Add</div>
-          </div>
-        </q-card-section>
+            
+            <button
+              class="button-add"
+              type="submit" 
+              >
+              ADD
+            </button>
+            
+          </q-card-section>
+        </form>
       </q-card>
     </q-dialog>
-
   </div>
 </template>
 
@@ -52,49 +68,71 @@
 import { mapGetters, mapActions } from "vuex";
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
-     return {
-
-     }
+    //object to submit
+    return {
+      expanseToSubmit: {
+        total: "200",
+        purchases: {
+          "": {
+            name: "",
+            description: "",
+            cost: "",
+            category: ""
+          }
+        }
+      }
+    };
   },
   computed: {
-    ...mapGetters('settings', ['settings']),
+    ...mapGetters("settings", ["settings"])
   },
   methods: {
-    ...mapActions('settings', ['setShowAddExpense'])
+    ...mapActions("settings", ["setShowAddExpense"]),
+
+    //add new expense
+
+    ...mapActions("expenses", ["addExpense"]),
+    submitForm() {
+      //add validate
+      this.submitExpense();
+    },
+    submitExpense() {
+      this.addExpense(this.expanseToSubmit);
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-
 // w/o dark mode
 .main-app {
   padding-top: 2rem;
   color: $primary;
   background: $for-background;
-  transition: background .5s ease-in-out;
+  transition: background 0.5s ease-in-out;
 }
 
 //with dark mode
 .dark {
   color: $primary-dark;
   background: $for-background-dark;
-  transition: color .5s ease-in-out;
+  transition: color 0.5s ease-in-out;
 }
 
 //styling add window
 .q-card {
   background: $for-background-dark;
-  opacity: .85;
+  opacity: 0.85;
   color: $for-white;
   border-radius: 20px !important;
-  
+
+  padding-top: 1.6rem;
+  padding-bottom: 1.6rem;
 }
 
 .q-dialog__backdrop {
-  
 }
 
 .blur-overlay {
@@ -105,7 +143,7 @@ export default {
 //inside dialog window
 //header
 
-//add and split 
+//add and split
 .header {
   text-align: center;
   color: $for-white;
@@ -118,41 +156,65 @@ export default {
   display: inline-block;
   padding-left: 1rem;
   padding-right: 1rem;
-  padding-top: 1rem;
+}
+
+.input-fiels-container {
+  margin-top: 2.6rem;
+  margin-bottom: 1.6rem;
+  width: 76vw;
+
+  padding: 0rem 1rem 0rem 1rem;
 }
 
 //input field style
 .input-field {
   background: rgba(0, 0, 0, 0.25);
   border-radius: 12px;
-  
-  width: 68vw;
-  margin: 0rem 1rem 1rem 1rem;
-  padding: 0.5625rem 1rem;
+  border-width: 2px;
+  border-color: rgba(0, 0, 0, 0);
+
+  width: 100%;
+
+  margin-bottom: 1rem;
+  padding: 0.6rem 0.875rem;
 
   font-weight: 500;
-  color: $secondary;
+  color: $for-white;
+
+  &::placeholder {
+    color: $secondary;
+    font-weight: 500;
+  }
+}
+
+//focus text field CHANGE THIS FRADIENT
+.input-field:focus {
+  border-color: $for-white !important;
+  border-radius: 12px;
+  outline: none;
+}
+
+//buttons
+.buttons {
+  
 }
 
 //button-add-style
 .button-add {
   background-color: $add-button;
+  color: $for-white;
 
   border-radius: 12px;
   width: 7.25rem;
 
-  margin: 2rem 1rem 1rem 1rem;
-  padding: 0.5625rem 1rem;
+  margin: 0rem 1rem 0rem 1rem;
+  padding: 0.6rem 0.875rem;
 
   font-weight: 600;
   text-transform: uppercase;
   text-align: center;
+
+  border: none;
 }
-
-
-.input-field-text {
-  
-}
-
 </style>
 
