8ddbc18b4ff44c5b14411ef547468b71f38e324f
bacebu4 <vasua14735@icloud.com>
2020-07-29 10:17:29 +0300

    back button is working now

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,25 +1,12 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops! Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
-    </div>
+<div>
+  <div class="content">
+    <img src="../assets/404.svg" width="60%">
   </div>
+  <div class="title-404">
+    Oops! Nothin here...
+  </div>
+</div>
 </template>
 
 <script >
@@ -27,3 +14,24 @@ export default {
   name: 'Error404'
 }
 </script>
+
+<style lang="scss">
+
+body {
+  background-color: #EFF0F2;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  margin-top: 25vh;
+}
+
+.title-404 {
+  margin-top: 2rem;
+  font-size: 1.5rem;
+  display: flex;
+  justify-content: center;
+}
+
+</style>
